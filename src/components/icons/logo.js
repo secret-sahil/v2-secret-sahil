3cde84c44a0438f10a1a2eaaef63e872d7b106a4
Sahil Kumar <sahilkumar142002@gmail.com>
2022-01-17 10:35:58 +0530

    modified logo size

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <h1><a>mrsahil.in</a></h1>
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+      <text x="-5" y="50" fill="currentColor" fontSize="14px">
+          mrsahil.in
+        </text>
+      </g>
+    </g>
+  </svg>
 );
 
 export default IconLogo;
