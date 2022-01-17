9dd211ee1cd9724ed1cc7a0966d11b42c8af860d
Sahil Kumar <sahilkumar142002@gmail.com>
2022-01-18 00:55:40 +0530

    updated google tracking id in gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,7 +151,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: '',
+        trackingId: 'UA-45666519-2',
       },
     },
   ],
